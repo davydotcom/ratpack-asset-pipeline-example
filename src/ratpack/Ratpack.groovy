536ebb34962dd61de3596e1e3f7f9a52f2f12ffe
Jacob Aae Mikkelsen <git@grydeske.dk>
2016-10-31 21:33:43 +0100

    Also add in system properties for configuration

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,24 +1,22 @@
+import ratpack.config.ConfigDataBuilder
 import ratpack.groovy.template.MarkupTemplateModule
 import asset.pipeline.ratpack.AssetPipelineModule
 import asset.pipeline.ratpack.AssetPipelineHandler
-import static ratpack.groovy.Groovy.groovyMarkupTemplate
+
 import ratpack.config.ConfigData
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
   bindings {
     module MarkupTemplateModule
-    module(AssetPipelineModule) {
-        println it.dump()
-        it.url("/")
-        it.sourcePath("../../../src/assets") // FIXME: With this, it works with ./gradlew run - but not with shadowJar
-    }
+
+    ConfigData configData = ConfigData.builder().sysProps().build()
+    AssetPipelineModule.Config config = configData.get(AssetPipelineModule.Config)
+    config.sourcePath = "../../../src/assets"
+    moduleConfig(new AssetPipelineModule(), config)
   }
 
   handlers {
-    // get {
-    //   render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
-    // }
     //Served by default from /assets or in this example from the root url
     all(AssetPipelineHandler)
   }
